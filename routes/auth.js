2c445b6e3cf92cd4fe8b641a30579a9c9da64e77
sherrygelato <sherrygelato@kakao.com>
2021-12-07 18:04:13 +0900

    clear

--- routes/auth.js
+++ routes/auth.js
@@ -28,17 +28,42 @@ module.exports = function (passport) {
     response.send(html);
   });
 
-  // /login으로 로그인 정보를 보냈을 때
+  // // /login으로 로그인 정보를 보냈을 때
+  // router.post('/login_process',
+  //   // callback 함수가 passport
+  //   passport.authenticate('local', {
+  //     // local: id, pwd 로그인 방식
+  //     successRedirect: '/',
+  //     failureRedirect: '/login',
+  //     failureMessage: true,
+  //     successFlash: true
+  //   })
+  // );
+
+  // // session 파일에 로그인 정보가 저장이 되기도 하고 안되기도 하고
+  // // /login으로 로그인 정보를 보냈을 때
+  // router.post('/login_process',
+  //   // callback 함수가 passport
+  //   passport.authenticate('local', {
+  //     // local: id, pwd 로그인 방식
+  //     failureRedirect: '/login',
+  //     function(request, response) {
+  //       request.session.save(function () {
+  //         response.redirect(`/`);
+  //       })
+  //     }
+  //   })
+  // );
+
+  // session에 정보가 저장되지 않았음에도 불구 리디렉션
   router.post('/login_process',
-    // callback 함수가 passport
     passport.authenticate('local', {
-      // local: id, pwd 로그인 방식
-      successRedirect: '/',
-      failureRedirect: '/auth/login',
-      failureMessage: true,
-      successFlash: true
-    })
-  );
+      failureRedirect: '/auth/login'
+    }), (req, res) => {
+      req.session.save(() => {
+        res.redirect('/')
+      })
+  })
   
   // destroy : 세션이 삭제됨
   router.get('/logout', function (request, response) {
