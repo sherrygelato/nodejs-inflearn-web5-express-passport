1de0fa7801c8a6875a01076162765d3a85258bff
sherrygelato <sherrygelato@kakao.com>
2021-12-07 15:23:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# # inflearn : WEB5 node.js express passport
+- link : https://www.inflearn.com/course/WEB5-Express-Passport-js/
+- cf : https://opentutorials.org/course/3402
---
+++ routes/auth.js
@@ -0,0 +1,167 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var sanitizeHtml = require('sanitize-html');
+var template = require('../lib/template.js');
+
+// 이러한 정보는 밖으로 꺼내야 한다. 소스 코드 안에 ㄴㄴ
+// 비밀번호를 평문이 아닌 Hash 등 비밀번호 암호화 해야 한다.
+// 정보 관리자들도 알지 못해야 한다.
+// 여러 사람의 인증 시스템도 잘 생각해야 한다.
+var authData = {
+  email: 'test@example.com',
+  password: '1234321!',
+  nickname: 'sherrygelato'
+}
+
+router.get('/login', function(request, response){
+    var title = 'WEB - login';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+      <form action="/auth/login_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p>
+          <input type="submit" value="login">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
+});
+
+router.post('/login_process', function (request, response) {
+  var post = request.body;
+  var email = post.email;
+  var password = post.pwd;
+
+  if (email === authData.email && password === authData.password) {
+    // success login!
+    console.log('success login!')
+
+    // 세션 데이터 어떤 데이터 저장할 것인가?
+    // 로그인 상태 정보, 페이지 접근할 때마다 닉네임 같은 것 세션에 저장
+
+    request.session.is_logined = true;
+    request.session.nickname = authData.nickname;
+
+    // response.send('Welcome!')
+    request.session.save(function(){
+      response.redirect(`/`);
+    });
+  } else {
+    // failed login!
+    console.log('failed login!')
+    response.send('Who are you?')
+  }
+
+  // response.redirect(`/topic/${title}`);
+});
+  
+// destroy : 세션이 삭제됨
+router.get('/logout', function (request, response) {
+  request.session.destroy(function (err) {
+    response.redirect('/');
+  });
+})
+
+module.exports = router;
+  
+/*
+router.get('/create', function(request, response){
+    var title = 'WEB - create';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+      <form action="/topic/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
+  });
+  
+  router.post('/create_process', function(request, response){
+    var post = request.body;
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/topic/${title}`);
+    });
+  });
+  
+  router.get('/update/:pageId', function(request, response){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var list = template.list(request.list);
+      var html = template.HTML(title, list,
+        `
+        <form action="/topic/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+      );
+      response.send(html);
+    });
+  });
+  
+  router.post('/update_process', function(request, response){
+    var post = request.body;
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error){
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.redirect(`/topic/${title}`);
+      })
+    });
+  });
+  
+  router.post('/delete_process', function(request, response){
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+      response.redirect('/');
+    });
+  });
+  
+  router.get('/:pageId', function(request, response, next) { 
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      if(err){
+        next(err);
+      } else {
+        var title = request.params.pageId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description, {
+          allowedTags:['h1']
+        });
+        var list = template.list(request.list);
+        var html = template.HTML(sanitizedTitle, list,
+          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+          ` <a href="/topic/create">create</a>
+            <a href="/topic/update/${sanitizedTitle}">update</a>
+            <form action="/topic/delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+        );
+        response.send(html);
+      }
+    });
+  });
+  */
