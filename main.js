77f5a55d087cc1e2780548a868e1075791ddca6c
sherrygelato <sherrygelato@kakao.com>
2021-12-07 15:53:36 +0900

    3. passport 자격확인

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 
+var authData = {
+  email: 'test@example.com',
+  password: '1234321!',
+  nickname: 'sherrygelato'
+}
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -24,6 +30,58 @@ app.use(session({
 // 꼭꼭!! 세션 다음에 코드 작성
 var passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  // params가 username과 password로 되어있는데
+  // 이미 설정되어있는 email과 pwd로 바꿈
+  {
+    usernameField: 'email',
+    passwordField: 'pwd'
+  },
+  function (username, password, done) {
+    console.log('LocalStrategy', username, password)
+
+    if (username === authData.email) {
+      console.log(1)
+      if (password === authData.password) {
+        console.log(2)
+        return done(null, authData);
+      } else {
+        console.log(3)
+        return done(null, false, {
+          message: 'Incorrect password.'
+        });
+      }
+    } else {
+      console.log(4)
+      return done(null, false, {
+        message: 'Incorrect username.'
+      });
+    }
+
+  /*
+  // 상위 done이라는 함수가 어떻게 되는가에 따른 로그인 처리
+  User.findOne({ username: username }, function (err, user) {
+  
+    // 에러 발생 시 에러를 첫번째 인자로 주고 passport가 알아서 처리
+    if (err) { return done(err); }
+
+    // user가 없다면, 왜 실패 했는지 
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+
+    // 사용자가 있으나 password 틀림
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+
+    // 사용자에 대한 정보 
+    return done(null, user);
+  });
+  */
+}
+));
 
 // /login으로 로그인 정보를 보냈을 때
 app.post('/auth/login_process',
